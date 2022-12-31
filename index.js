d2e6f8dd74e0de104b10ea6da3b378bfcab96307
Rachel Howell <rac777blue@hotmail.com>
2022-12-31 17:20:41 +0000

    add no-cors mode to app restricted request

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const appResUrl = "https://sandbox.api.service.nhs.uk/hello-world/hello/applicat
 
 const userResButton = document.getElementById("user-restricted-btn")
 
-openButton.addEventListener("click", () => {
-    fetch(openUrl)
-        .then(res => res.json())
-        .then(data => console.log(data))
-})
+// openButton.addEventListener("click", () => {
+//     fetch(openUrl)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// })
 
 appResButton.addEventListener("click", () => {
     fetch(appResUrl, {
+        mode: "no-cors",
         headers: {
+            // "Access-Control-Request-Headers": "apikey",
             "apikey": "zdb9BLQ28GMcNNMtjWS7KSBhRzHF5j98",
-            "Access-Control-Allow-Credentials": true
+            // "Access-Control-Request-Method": "GET",
+            // "Access-Control-Allow-Origin": "null"
         }
     })
         .then(res => res.json())
